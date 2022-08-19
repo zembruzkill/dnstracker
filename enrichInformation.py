decd79ad1d6c78b125a3af9a1b60fd76937b040a
Luciano <lzembruzki@inf.ufrgs.br>
2022-08-19 16:13:28 -0300

    getasn

--- enrichInformation.py
+++ enrichInformation.py
@@ -1,4 +1,6 @@
 import json
+import csv
+import sys
 from datetime import datetime
 import requests
 import sqlite3
@@ -33,16 +35,27 @@ def updateQuery(query):
         print(response.text)
 
 
+def getAsn(ip, csvreader):
+    for row in csvreader:
+        for item in row:
+            print(item.split('|')[3])
+
+
 if __name__ == '__main__':
-    conn = create_connection()
-    cur = conn.cursor()
-    query = cur.execute("select * from dns_query") # This line performs query and returns json result
-    rows = query.fetchall()
-    addressList = open("addresses.txt", "w")
-    for row in rows:
-        if row[5] != "None":
-            addressList.write(str(row[5]+"\n"))
-        if row[6] != "None":
-            addressList.write(str(row[6]+"\n"))
+    with open('enriched.txt', 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+
+        conn = create_connection()
+        cur = conn.cursor()
+        query = cur.execute("select * from dns_query") # This line performs query and returns json result
+        rows = query.fetchall()
+        addressList = open("addresses.txt", "w")
+        for row in rows:
+            if row[5] != "None":
+                addressList.write(row[5]+"\n")
+                getAsn(row[5], csvreader)
+            if row[6] != "None":
+                addressList.write(row[6]+"\n")
+                getAsn(row[6], csvreader)
             
 
